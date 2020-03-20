7e4b91d1a675e1748cbcca6b0462525d6b4ea1f2
Galih Ashari R <45219820+asharilabs@users.noreply.github.com>
2020-03-20 19:17:46 +0700

    import file unity
    melakukan import beberapa file-folder penting dari projek unity

---
+++ Assets/Scripts/GerakPeluruScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GerakPeluruScript : MonoBehaviour {
+
+	public float sudutPelemparan;
+	public float kecepatanAwal;
+
+	private float waktu = 0;
+	private bool tembakan = false;
+
+	// Use this for initialization
+	void Start () {
+				
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//transform.position = PergerakanPeluru();		
+
+		if (tembakan == true)
+		{
+			if (transform.position.y >= 0)
+			{
+				waktu += Time.deltaTime;
+				transform.position = PergerakanPeluru(waktu,
+					kecepatanAwal,
+					sudutPelemparan);
+			}
+			else
+			{
+				#region Reset Parameter
+				tembakan = false;
+				transform.position = Vector3.zero;
+				waktu = 0; 
+				#endregion
+			}
+		}
+
+		if( Input.GetKeyDown(KeyCode.Space)) tembakan = true;
+		
+	}
+
+	Vector3 PergerakanPeluru(float _time, float _kecAwal, float _sudut)
+	{
+		// x
+		float x = _kecAwal * _time * Mathf.Cos(_sudut * Mathf.PI / 180);    // Vo . t . cos a
+		// y
+		float y = (_kecAwal * _time * Mathf.Sin(_sudut * Mathf.PI / 180)) -
+			(0.5f * 9.8f * Mathf.Pow(_time, 2));
+		// z
+		float z = 0;
+
+		return new Vector3(x, y, z);
+	}
+}
